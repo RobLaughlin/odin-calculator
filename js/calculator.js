c9842b618a85c591d7dffb06de54fe9e157e1921
Rob Laughlin <waterfallkicks@gmail.com>
2024-07-27 10:28:06 -0700

    Add more calculator buttons

--- js/calculator.js
+++ js/calculator.js
@@ -1,21 +1,27 @@
 function populateCalculatorButtons() {
     // Populate the calculator buttons top to bottom, left to right.
-    const SYMBOLS = "789x456-123+C0.=";
-    const ROW_WIDTH = 4;
-    const WIDTH = (100 * ROW_WIDTH / SYMBOLS.length).toString() + '%';
+    const SYMBOLS = [
+        ['7', '8', '9', 'x'],
+        ['4', '5', '6', '-'],
+        ['1', '2', '3', '+'],
+        ['0', '.', 'C', '/'],
+        ['1/x', 'x²', '√x', '=']];
+    const WIDTH = "25%";
     const BTN_HEIGHT = "100px";
 
     let calcBody = document.getElementById("CalculatorBody");
     for (let i = 0; i < SYMBOLS.length; i++) {
-        const symbol = SYMBOLS[i];
-        let btn = document.createElement("button");
-        btn.textContent = symbol;
-        btn.classList.add("calculatorBtn");
-        btn.value = symbol;
-        btn.style.width = WIDTH;
-        btn.style.boxSizing = "border-box";
-        btn.style.height = BTN_HEIGHT;
-        calcBody.appendChild(btn);
+        for (let j = 0; j < SYMBOLS[i].length; j++) {
+            const symbol = SYMBOLS[i][j];
+            let btn = document.createElement("button");
+            btn.textContent = symbol;
+            btn.classList.add("calculatorBtn");
+            btn.value = symbol;
+            btn.style.width = WIDTH;
+            btn.style.boxSizing = "border-box";
+            btn.style.height = BTN_HEIGHT;
+            calcBody.appendChild(btn);
+        }
     }
 }
 
