eb82477fdef32523774497a4d5f7699471f77910
Rob Laughlin <waterfallkicks@gmail.com>
2024-07-29 02:39:43 -0700

    Add operator functionality

--- js/calculator.js
+++ js/calculator.js
@@ -1,40 +1,87 @@
 let calculatorState = {
     leftOperand: 0,
     rightOperand: null,
-    display: 0
+    operator: null,
 };
 
 function operate(l, r, operator) {
     switch (operator) {
         case '+': return l+r;
         case '-': return l-r;
-        case '*': return l*r;
+        case 'x': return l*r;
         case '/':
-            if (r == 0) { alert("Cannot divide by 0."); return null; }
+            if (r == 0) { alert("Cannot divide by 0."); clearCalculator(); return null; }
             else        { return l / r; }
         default: return
     }
 }
 
+function updateDisplay() {
+    let displayText = document.getElementById("CalculatorDisplayText");
+    if (calculatorState.operator === null) {
+        displayText.textContent = calculatorState.leftOperand.toString();
+    }
+    else {
+        displayText.textContent = calculatorState.leftOperand.toString() + calculatorState.operator.toString();
+        if (calculatorState.rightOperand !== null) {
+            displayText.textContent += calculatorState.rightOperand.toString();
+        }
+    }
+}
 function digitClicked(e) {
     // Populate display if symbol is a digit
 
-    // Update calculator state and then redraw
-    let displayText = document.getElementById("CalculatorDisplayText");
-    const displayTextContent = displayText.textContent;
-    const lastDigit = displayTextContent[displayTextContent.length - 1];
-    const digit = e.target.value;
-
-    if (lastDigit === '0' && displayTextContent.length === 1) {
-        displayText.textContent = digit;
+    // First, update the calculator state then update the display.
+    if (calculatorState.operator === null) {
+        if (calculatorState.leftOperand === 0) {
+            calculatorState.leftOperand = Number(e.target.value);
+        }
+        else {
+            calculatorState.leftOperand = Number(calculatorState.leftOperand.toString() + e.target.value.toString());
+        }
+    }
+    else if (calculatorState.rightOperand === null) {
+        calculatorState.rightOperand = Number(e.target.value);
     }
     else {
-        displayText.textContent += digit;
+        calculatorState.rightOperand = Number(calculatorState.rightOperand.toString() + e.target.value.toString());
     }
 
-    calculatorState.display = Number(displayText.textContent);
+    updateDisplay();
 }
 
+function operatorClicked(e) {
+    const operator = e.target.value;
+    if (calculatorState.operator === null) {
+        calculatorState.operator = operator;
+    }
+    else if (calculatorState.rightOperand !== null){
+        const result = operate(calculatorState.leftOperand, calculatorState.rightOperand, calculatorState.operator);
+        if (result !== null) {
+            evaluate();
+            calculatorState.operator = operator;
+        }
+    }
+    updateDisplay();
+}
+
+function clearCalculator() {
+    calculatorState.leftOperand = 0;
+    calculatorState.operator = null;
+    calculatorState.rightOperand = null;
+    updateDisplay();
+}
+
+function evaluate() {
+    if (calculatorState.operator !== null && calculatorState.rightOperand !== null) {
+        const result = operate(calculatorState.leftOperand, calculatorState.rightOperand, calculatorState.operator);
+        calculatorState.leftOperand = result === null ? 0 : result;
+        calculatorState.operator = null;
+        calculatorState.rightOperand = null;
+    }
+
+    updateDisplay();
+}
 function populateCalculatorButtons() {
     // Populate the calculator buttons top to bottom, left to right.
     const SYMBOLS = [
@@ -60,6 +107,19 @@ function populateCalculatorButtons() {
 
             if (!isNaN(symbol)) {
                 btn.addEventListener("click", digitClicked);
+            }
+            else {
+                switch (symbol) {
+                    case "x":
+                    case "-":
+                    case "+":
+                    case "/":
+                        btn.addEventListener("click", operatorClicked); break;
+                    case "=":
+                        btn.addEventListener("click", evaluate); break;
+                    case "C":
+                        btn.addEventListener("click", clearCalculator); break;
+                }
             }
             calcBody.appendChild(btn);
         }
